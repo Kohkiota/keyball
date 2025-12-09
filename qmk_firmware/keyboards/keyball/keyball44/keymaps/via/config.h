a01b5152fe5de5073ae32f1165073b9b399badcf
Kohki Ota <komail9server@gmail.com>
2025-12-09 12:43:49 +0900

    ホールド判定時間を145msに

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -37,4 +37,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define TAPPING_TERM 145
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+#define RETRO_TAPPING
