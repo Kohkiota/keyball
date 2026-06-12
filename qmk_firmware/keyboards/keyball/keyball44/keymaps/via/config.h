8b3691aa2f22cf0a8dd73c22c218bc263b137fb6
Kohki Ota <komail9server@gmail.com>
2026-06-12 16:24:34 +0900

    135

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
-#define TAPPING_TERM 120
+#define TAPPING_TERM 135
 // #define TAPPING_TERM 145
 
 // ホールド後でも他キー入力が無ければタップに戻す
